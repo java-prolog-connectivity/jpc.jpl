fa06680e0ab18f259d005af407f1b13ee9777798
Sergio Castro <sergioc78@gmail.com>
2014-03-15 02:18:31 +0100

    improvements on the Prolog engine instantiations

--- src/test/java/org/jpc/examples/metro/jpl/StressTestJpl.java
+++ src/test/java/org/jpc/examples/metro/jpl/StressTestJpl.java
@@ -1,22 +1,18 @@
 package org.jpc.examples.metro.jpl;
 
 
-import static org.jpc.engine.provider.PrologEngineProviderManager.setPrologEngineProvider;
-import static org.junit.Assert.assertTrue;
-
-import org.jpc.engine.jpl.JplYapDriver;
-import org.jpc.engine.profile.LogtalkEngineProfile;
-import org.jpc.engine.provider.SimpleEngineProvider;
 import org.jpc.examples.metro.MetroExample;
 import org.jpc.examples.metro.StressTest;
+import org.jpc.util.config.EngineConfigurationManager;
 import org.junit.BeforeClass;
 
 public class StressTestJpl extends StressTest {
 
 	@BeforeClass
     public static void oneTimeSetUp() {
-		setPrologEngineProvider(new SimpleEngineProvider(new LogtalkEngineProfile(new JplYapDriver()).createPrologEngine()));
-		assertTrue(MetroExample.loadAll());
+		EngineConfigurationManager engineConfigurationManager = EngineConfigurationManager.createFromFile("jpc_yap_logtalk.settings");
+		EngineConfigurationManager.setDefault(engineConfigurationManager);
+		MetroExample.loadAll();
     }
 
 }
--- src/test/java/org/jpc/jpl/JplSwiLogtalkEngineTestSuite.java
+++ src/test/java/org/jpc/jpl/JplSwiLogtalkEngineTestSuite.java
@@ -1,8 +1,5 @@
 package org.jpc.jpl;
 
-import static org.jpc.engine.provider.PrologEngineProviderManager.setPrologEngineProvider;
-
-import org.jpc.engine.provider.SimpleEngineProvider;
 import org.jpc.util.config.EngineConfigurationManager;
 import org.junit.BeforeClass;
 
@@ -11,6 +8,5 @@ public class JplSwiLogtalkEngineTestSuite extends JplLogtalkEngineTestSuite {
 	public static void setUp() {
 		EngineConfigurationManager engineConfigurationManager = EngineConfigurationManager.createFromFile("jpc_swi_logtalk.settings");
 		EngineConfigurationManager.setDefault(engineConfigurationManager);
-		setPrologEngineProvider(new SimpleEngineProvider(EngineConfigurationManager.getDefault().getPrologEngineById("swi_logtalk")));
 	}
 }
