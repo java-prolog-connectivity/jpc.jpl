870e2bcb23c1db2ce08db971a39315c68f06fdae
Sergio Castro <Sergio.Castro@tomtom.com>
2017-01-17 14:34:52 +0100

    renaming classes reifying number terms

--- src/test/java/org/jpc/salt/jpl/JplTransformationTest.java
+++ src/test/java/org/jpc/salt/jpl/JplTransformationTest.java
@@ -7,8 +7,8 @@ import static org.junit.Assert.assertEquals;
 import org.jpc.salt.JpcTermWriter;
 import org.jpc.term.Atom;
 import org.jpc.term.Compound;
-import org.jpc.term.FloatTerm;
-import org.jpc.term.IntegerTerm;
+import org.jpc.term.Float;
+import org.jpc.term.Integer;
 import org.jpc.term.Term;
 import org.jpc.term.Var;
 import org.junit.Test;
@@ -16,7 +16,7 @@ import org.junit.Test;
 public class JplTransformationTest {
 
 	org.jpl7.Term t1Jpl = new org.jpl7.Compound("id", new org.jpl7.Term[] {new org.jpl7.Compound("name2", new org.jpl7.Term[] {new org.jpl7.Atom("atom1"), new org.jpl7.Integer(-10), new org.jpl7.Float(10.5), new org.jpl7.Variable("A"), new org.jpl7.Variable("_A")})});
-	Term t1Jpc = new Compound("id", asList(new Compound("name2", asList(new Atom("atom1"), new IntegerTerm(-10), new FloatTerm(10.5), new Var("A"), new Var("_A")))));
+	Term t1Jpc = new Compound("id", asList(new Compound("name2", asList(new Atom("atom1"), new Integer(-10), new Float(10.5), new Var("A"), new Var("_A")))));
 	
 	@Test
 	public void testJplToJpl() {
